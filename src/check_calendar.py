29711f32a6470eff4d76a626a62bce231f02b1be
Lorenzo <lorenzo.carbonell.cerezo@gmail.com>
2020-04-16 07:50:06 +0200

    fix a bug with add task

--- src/check_calendar.py
+++ src/check_calendar.py
@@ -39,7 +39,7 @@ class CheckCalendar(Gtk.Grid):
         """TODO: to be defined. """
         Gtk.Grid.__init__(self)
 
-        self.expander = Gtk.Expander.new(None)
+        self.expander = Gtk.Expander.new('')
         self.calendar = Gtk.Calendar()
         self.expander.add(self.calendar)
         self.attach(self.expander, 0, 0, 1, 1)
